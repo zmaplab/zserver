84772278ecb54a914897715561c8fbc87310ef67
Lewis Zou <zlzforever@163.com>
2022-08-11 11:14:20 +0800

    处理中文 orleans 数据表不存在的问题

--- src/ZServer.API/Extensions/OrleansExtensions.cs
+++ src/ZServer.API/Extensions/OrleansExtensions.cs
@@ -15,102 +15,102 @@ using ZServer.Grains.WMS;
 namespace ZServer.API.Extensions;
 
 public static class OrleansExtensions
+{
+    public static IHostBuilder ConfigureOrleans(this IHostBuilder builder)
     {
-        public static IHostBuilder ConfigureOrleans(this IHostBuilder builder)
+        builder.UseOrleans((context, siloBuilder) =>
         {
-            builder.UseOrleans((context, siloBuilder) =>
+            var connectString = context.Configuration["Orleans:ConnectionString"];
+            var invariant = context.Configuration["Orleans:Invariant"];
+
+            var assembly = Assembly.Load($"{invariant}");
+
+            var scriptPath = $"{invariant}.sql";
+            if (!File.Exists(scriptPath))
             {
-                var connectString = context.Configuration["Orleans:ConnectionString"];
-                var invariant = context.Configuration["Orleans:Invariant"];
+                throw new Exception("Invariant sql is missing");
+            }
 
-                var assembly = Assembly.Load($"{invariant}");
+            var sql = File.ReadAllText(scriptPath);
 
-                var scriptPath = $"{invariant}.sql";
-                if (!File.Exists(scriptPath))
+            var connectionType =
+                assembly.GetTypes().FirstOrDefault(x => x.IsAssignableTo(typeof(IDbConnection)));
+            if (connectionType == null)
+            {
+                throw new Exception("Invariant driver's connection type is missing");
+            }
+
+            using var conn =
+                (IDbConnection)Activator.CreateInstance(connectionType, connectString);
+            try
+            {
+                conn.Query($"SELECT * FROM orleansmembershiptable LIMIT 0");
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("\"orleansmembershiptable\" does not exist") || e.Message.Contains("不存在"))
                 {
-                    throw new Exception("Invariant sql is missing");
+                    conn.Execute(sql);
                 }
-
-                var sql = File.ReadAllText(scriptPath);
-
-                var connectionType =
-                    assembly.GetTypes().FirstOrDefault(x => x.IsAssignableTo(typeof(IDbConnection)));
-                if (connectionType == null)
+                else
                 {
-                    throw new Exception("Invariant driver's connection type is missing");
+                    throw;
                 }
+            }
 
-                using var conn =
-                    (IDbConnection)Activator.CreateInstance(connectionType, connectString);
-                try
+            siloBuilder
+                .AddMemoryGrainStorageAsDefault()
+                .UseAdoNetClustering(options =>
                 {
-                    conn.Query($"SELECT * FROM orleansmembershiptable LIMIT 0");
-                }
-                catch (Exception e)
+                    options.ConnectionString = context.Configuration["Orleans:ConnectionString"];
+                    options.Invariant = context.Configuration["Orleans:Invariant"];
+                })
+                .UseAdoNetReminderService(options =>
                 {
-                    if (e.Message.Contains("\"orleansmembershiptable\" does not exist"))
+                    options.ConnectionString = connectString;
+                    options.Invariant = context.Configuration["Orleans:Invariant"];
+                })
+                .Configure<SiloOptions>(options => options.SiloName = context.Configuration["Orleans:SiloName"])
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = context.Configuration["Orleans:ClusterId"];
+                    options.ServiceId = context.Configuration["Orleans:ServiceId"];
+                })
+                .Configure<EndpointOptions>(options =>
+                {
+                    var siloPort = int.Parse(context.Configuration["Orleans:SiloPort"]);
+                    var gatewayPort = int.Parse(context.Configuration["Orleans:GatewayPort"]);
+                    options.GatewayPort = gatewayPort;
+                    options.SiloPort = siloPort;
+
+                    var hostIp = Environment.GetEnvironmentVariable("HOST_IP");
+                    IPAddress ipAddress;
+                    if (string.IsNullOrWhiteSpace(hostIp))
                     {
-                        conn.Execute(sql);
+                        var ips = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result;
+                        ipAddress = ips.FirstOrDefault(x =>
+                            x.AddressFamily == AddressFamily.InterNetwork
+                            && !Equals(x, IPAddress.Loopback)
+                            && !Equals(x, IPAddress.Any)
+                            && !Equals(x, IPAddress.Broadcast)
+                            && !Equals(x, IPAddress.None));
                     }
                     else
                     {
-                        throw;
+                        ipAddress = IPAddress.Parse(hostIp);
                     }
-                }
 
-                siloBuilder
-                    .AddMemoryGrainStorageAsDefault()
-                    .UseAdoNetClustering(options =>
-                    {
-                        options.ConnectionString = context.Configuration["Orleans:ConnectionString"];
-                        options.Invariant = context.Configuration["Orleans:Invariant"];
-                    })
-                    .UseAdoNetReminderService(options =>
-                    {
-                        options.ConnectionString = connectString;
-                        options.Invariant = context.Configuration["Orleans:Invariant"];
-                    })
-                    .Configure<SiloOptions>(options => options.SiloName = context.Configuration["Orleans:SiloName"])
-                    .Configure<ClusterOptions>(options =>
-                    {
-                        options.ClusterId = context.Configuration["Orleans:ClusterId"];
-                        options.ServiceId = context.Configuration["Orleans:ServiceId"];
-                    })
-                    .Configure<EndpointOptions>(options =>
-                    {
-                        var siloPort = int.Parse(context.Configuration["Orleans:SiloPort"]);
-                        var gatewayPort = int.Parse(context.Configuration["Orleans:GatewayPort"]);
-                        options.GatewayPort = gatewayPort;
-                        options.SiloPort = siloPort;
-
-                        var hostIp = Environment.GetEnvironmentVariable("HOST_IP");
-                        IPAddress ipAddress;
-                        if (string.IsNullOrWhiteSpace(hostIp))
-                        {
-                            var ips = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result;
-                            ipAddress = ips.FirstOrDefault(x =>
-                                x.AddressFamily == AddressFamily.InterNetwork
-                                && !Equals(x, IPAddress.Loopback)
-                                && !Equals(x, IPAddress.Any)
-                                && !Equals(x, IPAddress.Broadcast)
-                                && !Equals(x, IPAddress.None));
-                        }
-                        else
-                        {
-                            ipAddress = IPAddress.Parse(hostIp);
-                        }
-
-                        options.AdvertisedIPAddress = ipAddress;
-                        options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, siloPort);
-                        options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, gatewayPort);
-                    })
-                    .ConfigureApplicationParts(parts =>
-                        parts.AddApplicationPart(typeof(WMSGrain).Assembly).WithReferences());
-                if (context.Configuration["Orleans:Dashboard"] == "True")
-                {
-                    siloBuilder.UseDashboard(options => { options.Port = 8182; });
-                }
-            });
-            return builder;
-        }
+                    options.AdvertisedIPAddress = ipAddress;
+                    options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, siloPort);
+                    options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, gatewayPort);
+                })
+                .ConfigureApplicationParts(parts =>
+                    parts.AddApplicationPart(typeof(WMSGrain).Assembly).WithReferences());
+            if (context.Configuration["Orleans:Dashboard"] == "True")
+            {
+                siloBuilder.UseDashboard(options => { options.Port = 8182; });
+            }
+        });
+        return builder;
     }
+}
