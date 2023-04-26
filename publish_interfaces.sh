a046855ead832b9ad375651b17a9b33ecf72bad5
Lewis Zou <zlzforever@163.com>
2023-04-26 09:33:06 +0800

    使用统一版本管理

--- publish_interfaces.sh
+++ publish_interfaces.sh
@@ -5,4 +5,4 @@ rm -rf src/ZServer.Interfaces/bin/Release
 
 dotnet build -c Release ZServer.sln
 dotnet pack -c Release ZServer.sln
-nuget push src/ZServer.Interfaces/bin/Release/*.nupkg -Source  https://api.nuget.org/v3/index.json
+dotnet nuget push src/ZServer.Interfaces/bin/Release/*.nupkg --skip-duplicate --source  https://api.nuget.org/v3/index.json -k $NUGET_KEY
