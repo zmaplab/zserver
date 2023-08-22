7d56f1ef2d05a8b9f46dd137c56a8122f3a434bd
Lewis Zou <zlzforever@163.com>
2023-08-22 11:08:12 +0800

    调整目录名称

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 docker build --target zserver -f Dockerfile -t zlzforever/zserver .
 docker push zlzforever/zserver
 
-docker buildx build --platform linux/arm64 --target zserver-api -f API.Dockerfile -t zlzforever/zserver-api:20230410 .
-docker push zlzforever/zserver-api:20230410
+docker buildx build --platform linux/arm64 --target zserver-api -f API.Dockerfile -t zlzforever/zserver-api:20230822 .
+docker push zlzforever/zserver-api:20230822
 
 docker build --target zserver-api -f API.Dockerfile -t zlzforever/zserver-api:20230410 .
 docker push zlzforever/zserver-api:20230410
--- src/ZServer.Tests/CacheTests.cs
+++ src/ZServer.Tests/CacheTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Xunit;
 
 namespace ZServer.Tests;
@@ -5,8 +6,14 @@ namespace ZServer.Tests;
 public class CacheTests
 {
     [Fact]
-    public void MulitiTasks()
+    public void AddOrUpdate()
     {
-       
+        ConcurrentDictionary<string, int> cache = new();
+        cache.AddOrUpdate("1", 1, (_, _) => 1);
+        Assert.Equal(1, cache["1"]);
+        cache.AddOrUpdate("1", 1, (_, _) => 2);
+        Assert.Equal(2, cache["1"]);
+        cache.AddOrUpdate("1", 3, (_, _) => 3);
+        Assert.Equal(3, cache["1"]);
     }
 }
