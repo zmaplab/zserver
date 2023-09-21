deb800796a44ef46efce2f8644ddebc0c9da0f32
Lewis Zou <zlzforever@163.com>
2023-09-22 00:37:25 +0800

    公开 settter，防止反序列化错误

--- src/ZServer.Interfaces/MapResult.cs
+++ src/ZServer.Interfaces/MapResult.cs
@@ -9,32 +9,32 @@ namespace ZServer.Interfaces
         /// <summary>
         /// 执行结果是否正确
         /// </summary>
-        public bool Success { get; private set; }
+        public bool Success { get; set; }
 
         /// <summary>
         /// 返回的消息
         /// </summary>
-        public string Message { get; private set; }
+        public string Message { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        public string Code { get; private set; }
+        public string Code { get; set; }
 
         /// <summary>
         /// A service to indicate to a client where in the client's request an exception was encountered
         /// </summary>
-        public string Locator { get; private set; }
+        public string Locator { get; set; }
 
         /// <summary>
         /// 内容格式
         /// </summary>
-        public string ImageType { get; private set; }
+        public string ImageType { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
-        public byte[] ImageBytes { get; private set; }
+        public byte[] ImageBytes { get; set; }
 
         public static MapResult Failed(string message, string code, string locator = null)
         {
