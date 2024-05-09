a7563f0265f004d4debdf84b29e19e8eddf43593
Lewis Zou <zlzforever@163.com>
2024-05-09 22:57:16 +0800

    注释无用代码

--- src/ZMap/Infrastructure/CSharpDynamicCompiler.cs
+++ src/ZMap/Infrastructure/CSharpDynamicCompiler.cs
@@ -4,19 +4,18 @@ public abstract class CSharpDynamicCompiler
 {
     private static CSharpDynamicCompiler _instance;
 
-    protected abstract Func<Feature, dynamic> BuildFunc(string script);
+    // protected abstract Func<Feature, dynamic> BuildFunc(string script);
     protected abstract Func<Feature, T> BuildFunc<T>(string script);
-    //public abstract Type BuildType(string script);
 
     protected abstract void Initialize();
 
-    private static readonly ConcurrentDictionary<string, dynamic>
-        FuncCache;
+    // private static readonly ConcurrentDictionary<string, dynamic>
+    //     FuncCache;
 
-    static CSharpDynamicCompiler()
-    {
-        FuncCache = new ConcurrentDictionary<string, dynamic>();
-    }
+    // static CSharpDynamicCompiler()
+    // {
+        // FuncCache = new ConcurrentDictionary<string, dynamic>();
+    // }
 
     public static void Load<T>() where T : CSharpDynamicCompiler
     {
@@ -37,26 +36,26 @@ public abstract class CSharpDynamicCompiler
         }
     }
 
-    public static Func<Feature, dynamic> GetOrCreateFunc(string expression)
-    {
-        if (string.IsNullOrWhiteSpace(expression) || _instance == null)
-        {
-            return null;
-        }
+    // public static Func<Feature, dynamic> GetOrCreateFunc(string expression)
+    // {
+    //     if (string.IsNullOrWhiteSpace(expression) || _instance == null)
+    //     {
+    //         return null;
+    //     }
+    //
+    //     return FuncCache.GetOrAdd(expression.GetHashCode().ToString(), _ => _instance.BuildFunc(expression));
+    // }
 
-        return FuncCache.GetOrAdd(expression.GetHashCode().ToString(), _ => _instance.BuildFunc(expression));
-    }
-
-    public static Func<Feature, T> GetOrCreateFunc<T>(string expression)
-    {
-        if (string.IsNullOrWhiteSpace(expression) || _instance == null)
-        {
-            return null;
-        }
-
-        return FuncCache.GetOrAdd($"{expression.GetHashCode()}:{typeof(T).FullName}",
-            _ => _instance.BuildFunc<T>(expression));
-    }
+    // public static Func<Feature, T> GetOrCreateFunc<T>(string expression)
+    // {
+    //     if (string.IsNullOrWhiteSpace(expression) || _instance == null)
+    //     {
+    //         return null;
+    //     }
+    //
+    //     return FuncCache.GetOrAdd($"{expression.GetHashCode()}:{typeof(T).FullName}",
+    //         _ => _instance.BuildFunc<T>(expression));
+    // }
 
     public static Func<Feature, T> GetOrCreateFuncWithoutCache<T>(string expression)
     {
